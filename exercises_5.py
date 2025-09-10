f16e8acd4159aa691445f664e6068fbb7ab38fec
Samuel Wlaschinský <88965419+wlaschinsky@users.noreply.github.com>
2025-09-10 15:37:31 +0200

    cviceni input

---
+++ exercises_5.py
@@ -0,0 +1,35 @@
+# a nejake komplexnejsi ulohy? Co kdyz zada neco jineho nez int, resi se to to v tehle kapitole nebo jindy? 
+# ---------------------------------------------
+# CVIČENÍ: input() a podmínky
+# ---------------------------------------------
+
+# 1) Věk
+# - zeptej se uživatele na věk
+# - pokud je 18 a více → vypiš "Dospělý"
+# - jinak → "Mladistvý"
+
+
+# 2) Heslo
+# - zeptej se na heslo
+# - pokud je prázdné → vypiš "Heslo chybí"
+# - pokud je "tajne" → vypiš "Přístup povolen"
+# - jinak → "Špatné heslo"
+
+
+# 3) Sudé nebo liché
+# - uživatel zadá celé číslo
+# - zjisti, jestli je číslo sudé nebo liché
+#   (nápověda: operátor % – zbytek po dělení)
+
+
+# 4) Desetinné číslo
+# - uživatel zadá číslo s desetinnou tečkou
+# - vypiš, jestli je větší než 100.0
+
+
+# 5) Text a členství
+# - uživatel zadá libovolný text
+# - vypiš "Obsahuje Python", pokud v textu je slovo "Python" (bez ohledu na velikost písmen)
+# - jinak vypiš "Neobsahuje Python"
+
+
